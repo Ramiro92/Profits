29883e70764e9275334e08bafec9f0e27a46224a
Ramiro Amaya <ramiro_amaya16@hotmail.com>
2018-12-16 15:47:03 -0600

    modificaciones pequeñas

--- index.php
+++ index.php
@@ -21,20 +21,25 @@
  		<div class="col-md-12">
  			<h1>Bienvenidos</h1>
  			<br>
- 			<h3 class="text-justify">Tu visita es importante para nosotros, estamos para servirte en asesoría y educación financiera. 
+ 			<br>
+ 			<h5 class="text-justify">Tu visita es importante para nosotros, estamos para servirte en asesoría y educación financiera. 
 Te invitamos a navegar por todo el sitio. 
-</h3>
+</h5>
 		</div>
-		<br>
 		<div class="col-md-6">
+			<br>
+			<br>
 			<img src="IMG/para enlazar con el curso.png" class="img-fluid" alt="Responsive image">
 		</div>
 		<div class="col-md-6">
+			<br>
+			<br>
 			<img src="IMG/para enlazar con los brokers.png" class="img-fluid" alt="Responsive image">
 		</div>
 		</div>
 		</div>
 </header>
+<br>
 <br>	
  		<?php 
  			include("footer.php");
