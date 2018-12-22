191261d75d8449233b94416ab4ea69d4e361880d
Ramiro Amaya <ramiro_amaya16@hotmail.com>
2018-12-21 22:26:44 -0600

    modificaciones sobre.php

--- sobre.php
+++ sobre.php
@@ -26,6 +26,7 @@
 </div>
 <div class="col-md-9" data-spy="scroll" data-target="#list-example" data-offset="" class="scrollspy-example">
   <center><h4 id="list-item-1">¿Quién es Alfredo Martínez?</h4>
+    <br>
   <img id="imagen" src="IMG/alf.jpg" class="img-fluid" alt="Responsive image"></center>
   <br>
   <p class="text-justify">Un hombre emprendedor, visionario, profesional en Ciencias Humanas, con un historial de nueve años de experiencia en docencia y siete años de experiencia en el mercado bursátil de divisas.</p>
@@ -36,41 +37,49 @@
   </p>
 <br>
   <center><h4 id="list-item-2">¿Cuál es la historia de Profits?</h4></center>
-  <p class="text-justify">La historia de Profits está estrechamente ligada a la de su autor. Todo comienza el año 2011 cuando Alfredo Martínez estaba pasando por una difícil crisis financiera. Él queriendo buscar una solución a su problema comenzó a probar el sistema de mercadeo multinivel pero no le dio resultados. Luego probó vender pollos, en este negocio le iba muy  bien y se mantuvo durante un año, sin embargo, no se pudo mantener por problemas ambientales.
+  <br>
+  <p class="text-justify">La historia de Profits está estrechamente ligada a la de su autor. Todo comienza el año 2011 cuando Alfredo Martínez había terminado sus estudios profesionales pero, como le sucede a casi todo profesional recién graduado, no encontraba un trabajo digno, por lo que se vio afectado por una difícil crisis financiera. 
   </p>
   <p class="text-justify"> 
-Alfredo Martínez tenía la convicción de que se podía vivir haciendo negocio online y, buscando oportunidades, se encontró con el mercado bursátil. El primer contacto lo tuvo con un bróker americano, hizo una pequeña inversión de $200.00 pero debido al nulo conocimiento sobre el tema dicha inversión iba en perdida por lo que decidió retirar los fondos que aún quedaban. Luego tuvo contacto con una persona que conocía más sobre el tema, aprendió bastante pero no lo suficiente como para ser rentable, esto pudo comprobarlo cuando, apoyado por una socia, hizo una inversión de $2000.00 de lo cual perdió el 50%.
+Sin embargo, Alfredo Martínez, tenía la convicción de que se podía lograr la libertad financiera haciendo negocio online y, buscando oportunidades, se encontró con el mercado bursátil. El primer contacto lo tuvo con un bróker americano, hizo una pequeña inversión de $200.00 pero debido al nulo conocimiento sobre el tema dicha inversión iba en pérdida por lo que decidió retirar los fondos que aún quedaban. Luego tuvo contacto con una persona que conocía más sobre el tema, aprendió bastante pero no lo suficiente como para ser rentable, esto pudo comprobarlo cuando, apoyado por una socia, hizo una inversión de $2,000.00 de lo cual sólo recuperó $800.00.
 </p>
 <p class="text-justify"> 
-Con este historial de perdidas constantes decidió contactar con profesionales en el tema y contactó con un español, especialista en el tema, que llevaba ya muchos años de experiencia y vivía del trading. Además, Tomó otros cursos pagados a precios muy altos. 
+Con este historial de pérdidas decidió aprender de los profesionales y contactó con un español, especialista en el tema, que tenía ya muchos años de experiencia y vivía del trading, tomó sus cursos gratuitos y pagados. Además, compró otros cursos a precios muy altos. 
 </p>
+<p class="text-justify">El haber recibido formación profesional, sobre trading en divisas, le permitió lograr la rentabilidad de modo eficiente y constante. </p>
+<p class="text-justify">Y una vez aprendido y practicado todo respecto al mercado bursátil de divisas, creó su propio sistema basado en la acción del precio. Probó el sistema tanto en cuentas de práctica como en cuentas reales  y comprobó que se logra una rentabilidad con el 80% de precisión, es decir, con el sistema se gana ocho de diez operaciones introducidas al mercado. Y, es en este momento cuando nace Profits, como un proyecto para compartir el sistema basado en la acción del precio, porque lo que es bueno debe compartirse para bien de otros, lo cual es parte de la responsabilidad social. </p>
+<p class="text-justify">Profits, ahora convertido en una entidad de servicio bursátil online, especialmente de divisas, se pone a tu disposición para orientarte, de manera que tengas claridad de la estructura y funcionamiento de este mercado, en vistas a lograr la rentabilidad.  </p>
 <br>
 <center><h5>Ejemplo de rentabilidad</h5></center>
+<br>
 <center><img src="IMG/resultados sistema.png" class="img-fluid" alt="Responsive image"></center>
-<p class="text-justify">Una vez aprendido todo respecto al mercado bursátil de divisas, creó su propio sistema basado en la acción del precio. Probó el sistema en cuentas de práctica, vio que era rentable con el 80% de precisión, es decir, con el sistema se gana ocho de diez operaciones introducidas al mercado. Por lo tanto no dudó en emplearlo para cuentas reales, logrando una alta rentabilidad. Y es en este momento cuando nace Profits, como un proyecto para compartir el sistema basado en la acción del precio.
-El proyecto Profits, ahora convertido en una entidad de servicio bursátil online, tiene como objetivo orientar a las personas emprendedoras e inversoras que se sienten atraídas por el mercado bursátil para que tengan claridad de la estructura y funcionamiento de dicho mercado, en vistas a lograr la rentabilidad.  
-</p>
+<br>
   <h4 id="list-item-3">Objetivo</h4>
+  <br>
   <p class="text-justify">El objetivo de Profits es orientar a los emprendedores e inversores que empatizan con el mercado bursátil para que conozcan la estructura, funcionamiento y logística de este mercado y, a la vez, logren la rentabilidad empleando el sistema basado en la acción del precio.   </p>
   <h4 id="list-item-4">Valores</h4>
+  <br>
 <p class="font-weight-bold">
-1.  Respeto a la dignidad humana: <p class="text-justify"> Para Profits tú ocupas el primer lugar, se te reconocen tu valor, derechos y deberes.
+1.  Confianza: <p class="text-justify"> Generamos un ambiente de confianza entre las personas que prestan nuestros  servicios y el equipo de Profits. La base de nuestros servicios es la confianza porque estamos convencidos que toda persona merece lo mejor.  
 </p></p>
 <p class="font-weight-bold">
-2.  Transparencia e integridad: <p class="text-justify"> Profits es una entidad dedicada a orientar al emprendedor e inversor en materia bursátil, expresa en obras y palabras lo que cree. </p>
+2.  Respeto a la dignidad humana: <p class="text-justify"> Para Profits tú ocupas el primer lugar, sabemos lo que vales y por mismo te tratamos con respeto y reconocemos tus derechos.   </p>
 </p>
 <p class="font-weight-bold">
-3.  Ética y profesionalidad: <p class="text-justify">Se fundamenta en la ética y brinda los servicios de modo profesional.</p>
+3.  Honestidad:  <p class="text-justify">En Profits te ofrecemos servicios honestos sin intención oculta de engaño o falsedad.</p>
 </p>
 <p class="font-weight-bold">
-4.  Calidad de servicio: <p class="text-justify">El servicio que se presta es profesional, completo y actualizado.
+4.  Transparencia: <p class="text-justify">Profits es una entidad dedicada a orientar al emprendedor e inversor en materia bursátil, presenta lo más claro posible lo que realmente es el mercado bursátil con sus aspectos positivos y negativos, con sus ventajas y desventajas. 
 </p></p>
 <p class="font-weight-bold">
-5.  Solidaridad: <p class="text-justify">Nos solidarizamos con las personas que buscan una respuesta a sus inquietudes emprendedoras y les ofrecemos una solución adaptada a sus necesidades.</p>
+5.  Ética y profesionalidad: <p class="text-justify">Se fundamenta en la ética y brinda los servicios de modo profesional.   </p>
 </p>
 <p class="font-weight-bold">
-6.  Responsabilidad social y ambiental: <p class="text-justify"> Promovemos el respeto y cuido de la ecología humana, como también el respeto al medio ambiente.</p>
+6.  Calidad de servicio: <p class="text-justify"> El servicio que se presta es profesional, completo y actualizado. </p>
 </p>
+<p class="font-weight-bold">
+7.  Solidaridad: </p><p class="text-justify">Nos solidarizamos con las personas que buscan una respuesta a sus inquietudes emprendedoras y les ofrecemos una solución adaptada a sus necesidades. </p>
+<p class="font-weight-bold">8.  Responsabilidad social y ambiental: </p><p class="text-justify">Promovemos el respeto y cuido de la ecología humana, como también el respeto al medio ambiente.</p>
 </div>
       </div>
     </div>
