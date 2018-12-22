99f80decdf6ee6f69790fc72149ca565bfa6b41d
Ramiro Amaya <ramiro_amaya16@hotmail.com>
2018-12-21 23:55:52 -0600

    brokers

--- broker.php
+++ broker.php
@@ -13,10 +13,42 @@
 		<?php 
 			include("menu.php");
 		 ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<br>
+					<h1><center>TRADEVIEW MARKETS </center></h1>
+					<br>
+					<center><a href="https://www.tradeviewforex.com/Forms-accounts/individual.php?ib=1292"><img src="IMG/Imagen 1 Tradeview.png" class="img-fluid" alt="Responsive image"></a></center>
+					<br>
+					<p>Naturaleza: STP/ECN</p>
+					<p>Regulación: CIMA, bajo Licence 585163, Tradeview Ltd.</p>
+					<p>Oficinas: en Islas Caymán, Estados Unidos y Perú</p>
+					<p>Contacto: 50361805947</p>
+					<a href="https://www.tradeviewforex.com/Forms-accounts/individual.php?ib=1292" class="btn btn-primary" role="button" aria-pressed="true">ABRE TU CUENTA REAL AHORA</a>
+				</div>
+				<div class="col-md-6">
+					<br>
+					<h1><center>SWISSQUOTE </center></h1>
+					<br>
+					<center><a href="https://apply.swissquote.com/fx/?lang=es&partnerid=61b3f7b9-ba7f-4823-97e8-14b80c014ba0&formName=MT4LIVEINDIV"><img src="IMG/Imagen 2 Swissquite.jpg" class="img-fluid" alt="Responsive image"></a></center>
+					<br>
+					<p>Naturaleza: STP</p>
+					<p>Regulación: Suiza </p>
+					<p>Oficinas: en Suiza</p>
+					<p>Contacto: 50361805947</p>
+					<a href="https://apply.swissquote.com/fx/?lang=es&partnerid=61b3f7b9-ba7f-4823-97e8-14b80c014ba0&formName=MT4LIVEINDIV" class="btn btn-primary" role="button" aria-pressed="true">ABRE TU CUENTA REAL AHORA</a>
+					<br>
+					<br>
+					<a href="https://es.swissquote.com/forex/open-forex-demo-account?partnerid=61b3f7b9-ba7f-4823-97e8-14b80c014ba0&platform=MT4" class="btn btn-primary" role="button" aria-pressed="true">ABRE TU CUENTA DE PRÁCTICA GRATIS  </a>
+				</div>
+			</div>
+			<br>
+		</div> 
 	</header>
-	<?php 
-		include("footer.php");
-	 ?>
+		<?php 
+			include("footer.php");
+		 ?>
 </body>
 <script src="JS/jquery.js" type="text/javascript"></script>
 <script src="JS/bootstrap.js" type="text/javascript"></script>
